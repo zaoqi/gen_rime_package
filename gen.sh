f3759b5c5f430f8c5ff864346b236ebd869c6def
zaoqi <zaomir@outlook.com>
2019-07-21 17:54:57 +0800

    创建tgz和zip

--- gen.sh
+++ gen.sh
@@ -7,6 +7,11 @@ curl https://codeload.github.com/rime/rime-prelude/tar.gz/master | tar -xzv &
 curl https://codeload.github.com/rime/rime-essay/tar.gz/master | tar -xzv &
 curl https://codeload.github.com/rime/rime-terra-pinyin/tar.gz/master | tar -xzv &
 wait
+
 rm -fr */*/ */AUTHORS */README.md */LICENSE
 mv */* .
 rm -fr */
+
+7z a ../rime.zip . &
+tar -czvf ../rime.tgz . &
+wait
